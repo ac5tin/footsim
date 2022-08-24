4850bdc94d6b4411d17cf20526ce9ecc34be7d2b
austin <austinchang4@gmail.com>
2022-08-24 14:58:22 +0800

    feat: positions

---
+++ src/core/position.rs
@@ -0,0 +1,16 @@
+pub enum Position {
+    Goalkeeper,
+    LeftBack,
+    CenterBack,
+    RightBack,
+    LeftWingBack,
+    RightWingBack,
+    DefensiveMidfield,
+    CenterMidfield,
+    AttackingMidfield,
+    LeftMidfield,
+    RightMidfield,
+    LeftWing,
+    RightWing,
+    Striker,
+}
