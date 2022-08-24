28fdf826a02945d31e798d1271d7e8c44e2cb429
austin <austinchang4@gmail.com>
2022-08-24 14:58:07 +0800

    feat: manager

---
+++ src/core/manager.rs
@@ -0,0 +1,10 @@
+pub struct Manager {
+    pub id: u32,
+    pub name: String,
+    pub team_id: u32,
+    pub country_id: u32,
+    // mental
+    pub tactical: u8,
+    pub management: u8,
+    pub coaching: u8,
+}
